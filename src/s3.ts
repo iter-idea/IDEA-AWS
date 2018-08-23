7206af0106293fb406548f00094b1797c19c4363
MC <mc@matteocarbone.com>
2018-08-23 15:17:53 +0200

    Fixed createDownloadURLFromData

--- src/s3.ts
+++ src/s3.ts
@@ -7,8 +7,8 @@ import IdeaX = require('idea-toolbox');
 export class S3 {
   protected s3: any;
 
-  protected DEFAULT_DOWNLOAD_BUCKET: string = 'idea-downloads';
   protected DEFAULT_DOWNLOAD_BUCKET_PREFIX: string = 'common';
+  protected DEFAULT_DOWNLOAD_BUCKET: string = 'idea-downloads';
   protected DEFAULT_DOWNLOAD_BUCKET_SEC_TO_EXP: number = 180;
   protected DEFAULT_UPLOAD_BUCKET_SEC_TO_EXP: number = 300;
 
@@ -22,27 +22,36 @@ export class S3 {
   /**
    * Create a download link of a piece of data (through S3).
    * *Pratically*, it uploads the file on an S3 bucket, generating and returning a url to it.
-   * @param {string} prefix the bucket folder (e.g. the project name)
-   * @param {string} key the unique filepath in which to store the file
    * @param {any} data usually a buffer
-   * @param {string} contentType e.g. application/json
-   * @param {number} secToExp seconds to url expiration
-   * @param {string} bucket an alternative Downloads bucket to IDEA's default one
+   * @param {any} options strucuted as follows
+   ```
+    bucket?: string;       // downloads bucket; default: `idea-downloads`.
+    prefix?: string;       // folder (e.g. the project name); default: `common`.
+    key?; string;          // the unique filepath in which to store the file; default: _random_.
+    contentType?: string;  // e.g. application/json; default: _guessed_.
+    secToExp?: number;     // seconds to URL expiration; default: `180`.
+   ```
    * @return {Promise<string>}
    */
-  public createDownloadUrlFromData(
-    prefix: string, key: string, data: any, contentType?: string, secToExp?: number, bucket?: string
-  ): Promise<string> {
+  public createDownloadURLFromData(data: any, options?: any) {
     return new Promise((resolve, reject) => {
-      key = `${prefix || this.DEFAULT_DOWNLOAD_BUCKET_PREFIX}/${key}`;
-      bucket = bucket || this.DEFAULT_DOWNLOAD_BUCKET;
-      secToExp = secToExp || this.DEFAULT_DOWNLOAD_BUCKET_SEC_TO_EXP;
-      this.s3.upload({ Bucket: bucket, Key: key, Body: data, ContentType: contentType },
+      // if needed, randomly generates the key
+      if(!options.key)
+        options.key = new Date().getTime().toString().concat(Math.random().toString(36).slice(2));
+      // add the prefix to the key
+      options.key = `${options.prefix || this.DEFAULT_DOWNLOAD_BUCKET_PREFIX}/${options.key}`;
+      // set the other parameters
+      options.bucket = options.bucket || this.DEFAULT_DOWNLOAD_BUCKET;
+      options.secToExp = options.secToExp || this.DEFAULT_DOWNLOAD_BUCKET_SEC_TO_EXP;
+      // upload the file to the downloads bucket
+      this.s3.upload({
+        Bucket: options.bucket, Key: options.key, Body: data, ContentType: options.contentType
+      },
       (err: Error, data: any) => {
         IdeaX.logger('S3 UPLOAD', err, data);
         if(err) reject(err);
-        else resolve(this.s3.getSignedUrl('getObject', {
-          Bucket: bucket, Key: key, Expires: secToExp
+        else resolve(this.s3.getSignedURL('getObject', {
+          Bucket: options.bucket, Key: options.key, Expires: options.secToExp
         }));
       });
     });
@@ -54,7 +63,7 @@ export class S3 {
    * @param {string} key
    * @param {number} expires seconds after which the signed URL expires
    */
-  public signedUrlPut(bucket: string, key: string, expires?: number): string {
+  public signedURLPut(bucket: string, key: string, expires?: number): string {
     return this.s3.getSignedUrl('putObject', {
       Bucket: bucket, Key: key, Expires: expires || this.DEFAULT_UPLOAD_BUCKET_SEC_TO_EXP
     });
@@ -66,7 +75,7 @@ export class S3 {
    * @param {string} key
    * @param {number} expires seconds after which the signed URL expires
    */
-  public signedUrlGet(bucket: string, key: string, expires?: number): string {
+  public signedURLGet(bucket: string, key: string, expires?: number): string {
     return this.s3.getSignedUrl('getObject', {
       Bucket: bucket, Key: key, Expires: expires || this.DEFAULT_DOWNLOAD_BUCKET_SEC_TO_EXP
     });
