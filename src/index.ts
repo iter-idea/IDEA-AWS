5bb0a3434f8067e83361400e5828e2dbc5f8400a
MC <mc@matteocarbone.com>
2018-08-23 16:14:18 +0200

    Added signedURL to exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { ResourceController, ResourceControllerOptions } from './resourceController';
 export { DynamoDB } from './dynamoDB';
 export { Cognito } from './cognito';
-export { S3 } from './s3';
+export { S3, SignedURL } from './s3';
 export { SES } from './ses';
 export { SNS } from './sns';
