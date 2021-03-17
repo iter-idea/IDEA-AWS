b5913acc9403920c01b9523b76de5cc3ab679a8d
Matteo Carbone <mc@matteocarbone.com>
2021-03-17 18:25:58 +0100

    Fix on return data + refactoring

--- src/comprehend.ts
+++ src/comprehend.ts
@@ -1,44 +1,31 @@
-import { Comprehend as AmazonComprehend } from 'aws-sdk';
+import { AWSError, Comprehend as AmazonComprehend } from 'aws-sdk';
 
 /**
  * A wrapper for Amazon Comprehend.
  */
 export class Comprehend {
+  /**
+   * The instance of Comprehend.
+   */
   protected comprehend: AmazonComprehend;
-  /**
-   * The language of the input documents. You can specify any of the primary languages supported by Amazon Comprehend.
-   * All documents must be in the same language.
-   */
-  public languageCode: string;
-  /**
-   * A UTF-8 text string. Each string must contain fewer that 5,000 bytes of UTF-8 encoded characters.
-   */
-  public text: string;
 
-  /**
-   * Initialize a new Comprehend helper object.
-   */
   constructor() {
     this.comprehend = new AmazonComprehend({ apiVersion: '2017-11-27' });
   }
 
   /**
    * Inspects text and returns an inference of the prevailing sentiment (POSITIVE, NEUTRAL, MIXED, or NEGATIVE).
-   * @param params the parameters for detectSentiment
    */
   public detectSentiment(params: ComprehendParameters): Promise<string> {
     return new Promise((resolve, reject) => {
-      // load source and target languages codes
-      if (params.languageCode) this.languageCode = params.languageCode;
-      if (params.text) this.text = params.text;
       // check for obligatory params
-      if (!this.languageCode || !this.text) return reject();
+      if (!params.languageCode || !params.text) return reject(new Error('MISSING_PARAMETERS'));
       // execute the sentiment detection
       this.comprehend.detectSentiment(
         { LanguageCode: params.languageCode, Text: params.text },
-        (err: Error, data: any) => {
+        (err: AWSError, data: AmazonComprehend.DetectSentimentResponse) => {
           if (err) reject(err);
-          else resolve(data);
+          else resolve(data.Sentiment);
         }
       );
     });
