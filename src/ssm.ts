422177a5f622ce27c7c378b445ca8b6a20680901
Matteo Carbone <mc@matteocarbone.com>
2024-01-09 14:36:27 +0100

    feat(ssm): internal cache to avoid multiple requests

--- src/ssm.ts
+++ src/ssm.ts
@@ -5,6 +5,7 @@ import * as AWSSystemsManager from '@aws-sdk/client-ssm';
  */
 export class SystemsManager {
   protected ssm: AWSSystemsManager.SSMClient;
+  protected cache = new Map<string, string>();
 
   constructor() {
     this.ssm = new AWSSystemsManager.SSMClient();
@@ -13,17 +14,20 @@ export class SystemsManager {
   /**
    * Get a parameter by its name (path).
    */
-  async getParameterByName(name: string): Promise<string> {
-    const command = new AWSSystemsManager.GetParameterCommand({ Name: name });
-    const result = await this.ssm.send(command);
-    return result.Parameter.Value;
+  async getParameterByName(
+    name: string,
+    options: { noCache?: boolean; withDecryption?: boolean } = {}
+  ): Promise<string> {
+    if (!options.noCache && this.cache.has(name)) return this.cache.get(name);
+    const command = new AWSSystemsManager.GetParameterCommand({ Name: name, WithDecryption: options.withDecryption });
+    const { Parameter } = await this.ssm.send(command);
+    this.cache.set(name, Parameter.Value);
+    return Parameter.Value;
   }
   /**
    * Get a parameter (with decryption) by its name (path).
    */
-  async getSecretByName(name: string): Promise<string> {
-    const command = new AWSSystemsManager.GetParameterCommand({ Name: name, WithDecryption: true });
-    const result = await this.ssm.send(command);
-    return result.Parameter.Value;
+  async getSecretByName(name: string, options: { noCache?: boolean } = {}): Promise<string> {
+    return this.getParameterByName(name, { ...options, withDecryption: true });
   }
 }
